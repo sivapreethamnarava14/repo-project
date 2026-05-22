686540ee36ce2430316fc36189ab98c09e8b241e
sivapreethamnarava14 <sivapreethamnarava14@gmail.com>
2026-05-23 00:26:17 +0530

    Add square root functionality and update menu options
    Advanced Python Calculator with multiple mathematical operations and user-friendly menu system.

--- calculator-project.py
+++ calculator-project.py
@@ -1,3 +1,5 @@
+import math
+
 print("========== ADVANCED CALCULATOR ==========")
 
 while True:
@@ -9,28 +11,46 @@ while True:
     print("4. Division")
     print("5. Power")
     print("6. Modulus")
-    print("7. Exit")
+    print("7. Square Root")
+    print("8. Exit")
 
-    choice = input("\nEnter your choice (1-7): ")
+    choice = input("\nEnter your choice (1-8): ")
 
-    if choice == "7":
-        print("\nThank you for using Calculator ")
+    # Exit
+    if choice == "8":
+        print("\nThank you for using Calculator")
         break
 
+    # Square Root
+    elif choice == "7":
+
+        num = float(input("Enter Number: "))
+
+        if num < 0:
+            print("Cannot find square root of negative number")
+
+        else:
+            print("Square Root =", math.sqrt(num))
+
+    # Other Operations
     elif choice in ["1", "2", "3", "4", "5", "6"]:
 
         a = float(input("Enter First Number: "))
         b = float(input("Enter Second Number: "))
 
+        # Addition
         if choice == "1":
             print("Result =", a + b)
 
+        # Subtraction
         elif choice == "2":
             print("Result =", a - b)
 
+        # Multiplication
         elif choice == "3":
             print("Result =", a * b)
 
+        # Division
         elif choice == "4":
 
             if b == 0:
@@ -39,11 +59,14 @@ while True:
             else:
                 print("Result =", a / b)
 
+        # Power
         elif choice == "5":
             print("Result =", a ** b)
 
+        # Modulus
         elif choice == "6":
             print("Result =", a % b)
 
+    # Invalid Choice
     else:
-        print("Invalid Choice")
+        print("Invalid Choice! Please select between 1 to 8.")
